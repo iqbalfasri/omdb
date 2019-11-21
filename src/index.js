ec45d50f5c360b5586b5cca5ec10771df899c3fe
Iqbal Fasri <iqbalx64@gmail.com>
2019-11-21 10:35:06 +0700

    create and use router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
+import App from "./app";
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  rootElement
+);
