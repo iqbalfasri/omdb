9931fe0ad86ea7a57ba97ef38b143d3f4b983d2f
Iqbal Fasri <iqbalx64@gmail.com>
2019-11-21 14:36:00 +0700

    create empty image

--- src/component/movies/card.js
+++ src/component/movies/card.js
@@ -9,11 +9,13 @@ function Card({ data }) {
         return (
           <a href={`/movie/detail/${movie.imdbID}`} className="col-md-4 mb-4">
             <div className="card card-custom text-white">
-              <img
-                style={{ maxHeight: "445px" }}
-                src={movie.Poster}
-                className="card-img-top"
-              />
+              {movie.Poster == "N/A" ? (
+                <div className="empty-image">
+                  <p>Not found.</p>
+                </div>
+              ) : (
+                <img style={{ maxHeight: "444px" }} src={movie.Poster} />
+              )}
               <div className="card-body">
                 <h5 className="card-title">{movie.Title}</h5>
                 <p className="card-text text-muted">{movie.Year}</p>
