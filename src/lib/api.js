7c0249bb2fb8941ac066b1a97da71cadfdcb18a5
Iqbal Fasri <iqbalx64@gmail.com>
2019-11-21 11:49:14 +0700

    fix cors

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "17179bd0";
-const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 export const searchMovies = async keyword => {
   let response = await axios.get(`${API_URL}&s=${keyword}`);
